1e9707e3d55cb5434a62b5a614936b64fb0eb793
mlgzacfly <mlgzackfly@gmail.com>
2024-03-17 01:51:38 +0800

    !feat: upload visualize code

--- visualize.py
+++ visualize.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
+# 設置中文字體
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']  # 請根據您的系統安裝的中文字體進行修改
+
 class CourseScraper:
     def __init__(self, url, headers):
         self.url = url
@@ -18,7 +20,7 @@ class CourseScraper:
         rows = table.find_all('tr')
 
         data = []
-        for row in rows:
+        for row in rows[2:]:  # 從第三行開始抓取，以跳過標題和表頭
             cols = [ele.text.strip() for ele in row.find_all('td')]
             data.append([col if col else " " for col in cols])
 
@@ -46,7 +48,7 @@ class CourseScraper:
 
     def scrape_courses(self, start_year, end_year):
         all_course_data = []
-        for year in range(start_year, end_year):
+        for year in range(start_year, end_year + 1):
             for semester in range(1, 3):
                 payload = self.generate_payload(year, semester)
                 course_data = self.fetch_course_data(payload)
@@ -56,17 +58,17 @@ class CourseScraper:
     def close_session(self):
         self.session.close()
 
-def plot_course_selection(course_data):
-    course_names = [course[7] for course in course_data]
-    selected_students = [int(course[14]) for course in course_data]
-    max_capacity = [int(course[15]) for course in course_data]
+def plot_course_selection(course_data, course_type):
+    course_names = [course[7] for course in course_data if course[11][0] == course_type]
+    selected_students = [int(course[14]) for course in course_data if course[11][0] == course_type]
+    max_capacity = [int(course[15]) for course in course_data if course[11][0] == course_type]
 
     plt.figure(figsize=(12, 6))
-    sns.barplot(x=course_names, y=selected_students, color='b', alpha=0.5, label='修課人數')
+    sns.barplot(x=course_names, y=selected_students, color='b', alpha=0.5, label='選課人數')
     sns.barplot(x=course_names, y=max_capacity, color='r', alpha=0.5, label='人數上限')
     plt.xlabel('課程名稱')
     plt.ylabel('學生人數')
-    plt.title('選課資訊')
+    plt.title(f'{course_type}修 課程選課情況')
     plt.xticks(rotation=90)
     plt.legend()
     plt.tight_layout()
@@ -77,10 +79,11 @@ def main():
     HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
 
     course_scraper = CourseScraper(URL, HEADERS)
-    course_data = course_scraper.scrape_courses(108, 112)
+    course_data = course_scraper.scrape_courses(108, 113)
     course_scraper.close_session()
 
-    plot_course_selection(course_data)
+    plot_course_selection(course_data, '必')
+    plot_course_selection(course_data, '選')
 
 if __name__ == "__main__":
     main()
